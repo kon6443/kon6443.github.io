5e0925b8e70bad2c4c9b249a1f88a9e7468b03b6
kon6443@gmail.com <kon6443@gmail.com>
2022-01-25 18:17:43 +0900

    submit progress

--- server.js
+++ server.js
@@ -39,30 +39,42 @@ app.get('/result/country/:country', function(req, res) {
 });
 
 app.get('/data', function(req, res) {
-    console.log('/data');
-    res.sendFile(__dirname + '/data.html');
+    res.sendFile(__dirname + '/data.html',{data:'this'});
 });
 
-app.get('/data/:name/:ssn', function(req, res) {
+app.get('/data/name/:name/ssn/:ssn', function(req, res) {
+    ///data/name/:name/ssn/:ssn
     //req.query.country
     //req.params.country
+    console.log('/data/name/:name/ssn/:ssn');
+    console.log('req.query: ', req.query);
+    console.log('req.query.name: ',req.query.name);
+    console.log('req.query.ssn: ',req.query.ssn);
     console.log('req.params: ',req.params);
     console.log('req.params.name: ',req.params.name);
     console.log('req.params.ssn: ',req.params.ssn);
+    //res.end('thank you.');
+
     var options = {
         mode: 'json',
         pythonPath:'',  
         pythonOptions:['-u'],
         scriptPath:'',
-        args: [req.params.name, req.params.ssn] 
+        args: [req.params.name] 
     };
     PythonShell.PythonShell.run('dbPrac.py', options, function(err, res) {
         if(err) throw err;
         console.log('res[0]', res[0]);
-        console.log('res[0][0]', res[0][0]);
-        console.log('res[0][1]', res[0][1]);
+        console.log('res[0][0][3]: ', res[0][0][3]);
+        //res.end('res[0][0][3]');
+        //res.status(200).send(res[0][0][3]);
+        //res.end('res[0][0][3]');
+        //console.log('res[0][0]', res[0][0]);
+        //console.log('res[0][1]', res[0][1]);
         //res.render({data:res[0]});
-        res.status(200).send(res[0]);
+        //res.status(200).send(res[0]);
     });
+    res.end('res[0][0][3]');
     //res.status(200).send(return_value);
 });
+
